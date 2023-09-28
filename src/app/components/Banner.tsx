53a99b6aa5ef605f0e5443c7a6b8e70f737265de
Ansari839 <abdullahansari839@gmail.com>
2023-09-28 10:18:39 +0500

    Navbar

---
+++ src/app/components/Banner.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className='bg-green-200 absolute top-0 h-screen w-screen'>
+      
+    </div>
+  )
+}
+
+export default Banner
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,39 +1,54 @@
 "use client"
 import React from 'react'
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
-} from "@/components/ui/navigation-menu"
 import './style.css'
+import Link from 'next/link'
 const Navbar = () => {
   return (
-    <div>
-      <NavigationMenu className='flex justify-center' >
-      <NavigationMenuList className='flex-1 text-blue-700'>
-          <NavigationMenuItem>
+    <nav className="border-gray-200 mb-10 bg-transparent sticky z-50">
+      <div className="w-full mx-auto p-8">
+        <div className="mx-2 flex flex-wrap items-center justify-between">
+          <Link href="#" className="flex">
             Logo
-          </NavigationMenuItem>
+          </Link>
           
-        </NavigationMenuList>
+          <div className="hidden md:flex justify-between items-end w-full md:w-auto md:order-1" id="mobile-menu-3">
+            <ul className="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
+              <li>
+                <Link href="#"
+                  className="text-green-800"
+                  aria-current="page">HOME</Link>
+              </li>
+              <li>
+                <Link href="#"
+                  className="text-green-800"
+                  >SHOP</Link>
+              </li>
+              <li>
+                <Link href="#"
+                  className="text-green-800">FEATURES</Link>            </li>
+              <li>
+                <Link href="#"
+                  className="text-green-800">CONTACT</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="hidden md:flex justify-between items-end w-full md:w-auto md:order-1" id="mobile-menu-3">
+            <ul className="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
+              <li>
+              <button>Login</button>
+              </li>
+              {/* <li>
+                <Link href="#"
+                  className="text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0">SHOP</Link>
+              </li> */}
+              
+            </ul>
+          </div>
+        </div>
+      </div>
+      
+    </nav>
 
-
-        <NavigationMenuList className='flex-3'>
-          <NavigationMenuItem className='nav-link'>
-            Item One
-          </NavigationMenuItem>
-          <NavigationMenuItem> 
-            Item Two
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
-
-    </div>
   )
 }
 
